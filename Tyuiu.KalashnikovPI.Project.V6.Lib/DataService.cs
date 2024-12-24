186edfb55b6bfe60f97d73f43709aa80a173d6a1
kalashnikovpi <kalashnicovp@yandex.ru>
2024-12-24 23:37:01 +0500

    bugfix

--- Tyuiu.KalashnikovPI.Project.V6.Lib/DataService.cs
+++ Tyuiu.KalashnikovPI.Project.V6.Lib/DataService.cs
@@ -11,30 +11,26 @@ namespace Tyuiu.KalashnikovPI.Project.V6.Lib
             {
                 throw new FileNotFoundException("Файл не найден.", filepath);
             }
-
-
-            string[] lines = File.ReadAllLines(filepath);
-
+            string[] lines = File.ReadAllLines(filepath).Where(line => !string.IsNullOrWhiteSpace(line)).ToArray();
             int rows = lines.Length;
-            int columns = lines[0].Split(';').Length; 
-
+            if (rows == 0)
+            {
+                throw new FormatException("Файл пуст.");
+            }
+            int columns = lines[0].Split(';').Length;
             string[,] data = new string[rows, columns];
-
             for (int i = 0; i < rows; i++)
             {
                 string[] values = lines[i].Split(';');
-
                 if (values.Length != columns)
                 {
                     throw new FormatException("Некорректный формат данных в строке: " + lines[i]);
                 }
-
                 for (int j = 0; j < columns; j++)
                 {
                     data[i, j] = values[j];
                 }
             }
-
             return data;
         }
     }
